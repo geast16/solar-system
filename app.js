642ae5cc8700ba749e1df29cecc6ff944c1998ae
Gregory East <gregoryeast1@outlook.com>
2026-06-01 21:13:54 -0400

    updated app.js

--- app.js
+++ app.js
@@ -11,16 +11,18 @@ app.use(bodyParser.json());
 app.use(express.static(path.join(__dirname, '/')));
 app.use(cors())
 
-mongoose.connect(process.env.MONGO_URI, {
+mongoose.connect("mongodb+srv://supercluster.d83jj.mongodb.net/superData", {
     user: process.env.MONGO_USERNAME,
     pass: process.env.MONGO_PASSWORD,
     useNewUrlParser: true,
     useUnifiedTopology: true
-}, err => {
+}, function (err) {
     if (err) {
-        console.error("MongoDB connection error:", err);
+        console.log("error!! " + err)
+    } else {
+        //  console.log("MongoDB Connection Successful")
     }
-});
+})
 
 var Schema = mongoose.Schema;
 
